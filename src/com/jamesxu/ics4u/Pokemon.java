8efcb85fccb69cf6fd62a2497efb1faf35a9e443
James Xu <xujames007@gmail.com>
2018-11-13 16:43:30 -0500

    Initial commit

---
+++ src/com/jamesxu/ics4u/Pokemon.java
@@ -0,0 +1,6 @@
+package com.jamesxu.ics4u;
+
+public class Pokemon {
+    private int hp, hpMax, energy;
+    private String name, type, resistance, weakness;
+}
---
+++ src/com/jamesxu/ics4u/Trainer.java
@@ -0,0 +1,10 @@
+package com.jamesxu.ics4u;
+
+import java.util.ArrayList;
+
+public abstract class Trainer {
+    String name;
+    Pokemon active;
+    ArrayList<Pokemon> arsenal;
+    
+}
